efbd0660146a5d21f4be3795432d20e764f31df5
Nikita <nikita.morozov.mnd@gmail.com>
2025-06-11 23:13:21 +0300

    test

--- OOPHomework/People.h
+++ OOPHomework/People.h
@@ -5,10 +5,15 @@ class People
     private:
         string name;
     public:
+        People(){}
         People(const string& name) : name(name) {}
         virtual string showInfo() const {
             string s = "name: " + name + "\n";
         }
-    
+        virtual int DangerLevel(string crime) {
+            return 0;
+        }
+        ~People() {}
+
 };
 
--- OOPHomework/prisoners.h
+++ OOPHomework/prisoners.h
@@ -7,17 +7,25 @@ private:
     Initialization init;
     int prisonerID = init.RandomID();
     string name = init.RandomName();
+    int age;
+    bool alive;
     string crime = init.RandomCrime();
+     
 public:
-    
+
+    Prisoner(){}
     Prisoner(const string& name, int id) : People(name), prisonerID(id) {}
     string showInfo() const override {
-        string s = "Prisoner: " + name + ", ID: " + to_string(prisonerID)
+      string s = "Prisoner: " + name + ", ID: " + to_string(prisonerID)
             + ", crime: " + crime;
         return s;
     }
+    int DangerLevel(string crime) {
 
 
+
+    }
+    ~Prisoner() {}
 
 };
 
